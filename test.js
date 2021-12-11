fb5a5026143b712fb5c3eae72763db88b90b0a87
Trinh Ngoc Minh <90078397+ngocminhvn@users.noreply.github.com>
2021-12-11 16:06:15 +0700

    Update and rename test.js to screen.js

--- test.js
+++
@@ -1,6 +0,0 @@
-function dark() {
-        var elements = document.getElementsByClassName("wrapper")
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].style.backgroundColor = "#000000";
-        }
-   }
