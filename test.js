6e3f4bddc671ecf58ecdd3cedb7a8aadb7394722
Trinh Ngoc Minh <90078397+ngocminhvn@users.noreply.github.com>
2021-12-11 15:56:31 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+function dark() {
+        var elements = document.getElementsByClassName("wrapper")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.background-color=black;
+        }
+   }
