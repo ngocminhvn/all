267258d724b81cb588db6ebbb0ab8c3bb4d6eee7
Trinh Ngoc Minh <90078397+ngocminhvn@users.noreply.github.com>
2021-12-20 18:26:49 +0700

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,8 +1,12 @@
 // kết hợp với swalalert
-// <link href='https://cdn.jsdelivr.net/npm/sweetalert2@11.3.0/dist/sweetalert2.min.css' rel='stylesheet'/>
-// <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11.3.0/dist/sweetalert2.min.js'></script>
-// <script src='https://cdn.jsdelivr.net/gh/ngocminhvn/all/copy.js' type='text/javascript'></script>
+/*
+<link href='https://cdn.jsdelivr.net/npm/sweetalert2@11.3.0/dist/sweetalert2.min.css' rel='stylesheet'/>
+<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11.3.0/dist/sweetalert2.min.js'></script>
+<script src='https://cdn.jsdelivr.net/gh/ngocminhvn/all/copy.js' type='text/javascript'></script>
+*/
 // Ex: <h1 onclick="copy(participle)">Click to copy participle </h1>
+
+
 function copy(text) {
   document.body.insertAdjacentHTML("beforeend","<div id=\"copy\" contenteditable>"+text+"</div>")
   document.getElementById("copy").focus();
